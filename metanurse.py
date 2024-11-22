428a2a31786ccdcbc9885f5313ebd1cc9d6cb7ec
beep boop <githubbot@vadim.me>
2024-11-23 00:19:39 +0100

    7 (0.0). Final reward: -199.6611094600782

--- metanurse.py
+++ metanurse.py
@@ -1,20 +1,21 @@
 import sys
 
-for line in sys.stdin:
-    (
-        alertness,
-        hypertension,
-        intoxication,
-        time_since_slept,
-        time_elapsed,
-        work_done,
-    ) = map(float, line.split())
 
-    if hypertension > 0.6 or intoxication > 0.4 or time_since_slept > 14:
-        print(3)  # sleep
-    elif alertness < 0.4 and time_since_slept < 10:
-        print(1)  # drink coffee and work
-    elif intoxication < 0.15 and hypertension < 0.5:
-        print(0)  # just work
-    else:
-        print(2)  # drink beer and work
+def decide_action(
+    alertness, hypertension, intoxication, time_since_slept, time_elapsed, work_done
+):
+    if hypertension > 0.6 or intoxication > 0.4:
+        return 3  # sleep
+    if time_since_slept > 14:
+        return 3  # sleep
+    if alertness < 0.5:
+        if alertness < 0.2:
+            return 3  # sleep if alertness is very low
+        return 1  # drink coffee and work
+    return 0  # just work
+
+
+for line in sys.stdin:
+    observations = list(map(float, line.strip().split()))
+    action = decide_action(*observations)
+    print(action)
