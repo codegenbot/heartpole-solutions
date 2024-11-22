5cdfc3b52b37e9c7e38f04a8a05e365685700d20
beep boop <githubbot@vadim.me>
2024-11-23 00:08:18 +0100

    1 (0.0). Write a program to solve the following task:
    -----
    You are a developer working on an important project. You want to maximize your productivity, but not at the expense of health - any serious health issue will negate all the effects of increased productivity.
    -----
    
    Your program will be run in the terminal once (include a loop if needed).
    At every step of the game (maximum of 1000 steps), it will receive a set of observations as input and it will have to output an action to be taken.
    The observations will be input as a space-separated list of 6 floating point observations: ['alertness', 'hypertension', 'intoxication',
     'time_since_slept', 'time_elapsed', 'work_done']
    
    In response, print ONLY the number of the action to be taken, from the list below:
    0 - just work
    1 - drink coffee and work
    2 - drink beer and work
    3 - sleep

--- metanurse.py
+++ metanurse.py
@@ -10,11 +10,11 @@ for line in sys.stdin:
         work_done,
     ) = map(float, line.split())
 
-    if hypertension > 0.7 or intoxication > 0.7 or time_since_slept > 16:
+    if hypertension > 0.7 or intoxication > 0.5 or time_since_slept > 16:
         print(3)  # sleep
-    elif alertness < 0.3:
+    elif alertness < 0.3 and time_since_slept < 8:
         print(1)  # drink coffee and work
-    elif intoxication < 0.3:
-        print(2)  # drink beer and work
-    else:
+    elif intoxication < 0.2:
         print(0)  # just work
+    else:
+        print(2)  # drink beer and work
