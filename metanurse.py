baeae8fd96ebb3109a07a52af49388444fb67d7a
beep boop <githubbot@vadim.me>
2024-11-23 00:10:43 +0100

    2 (0.0). Write a program to solve the following task:
    -----
    You are a developer working on an important project. You want to maximize your productivity, but not at the expense of health - any serious health issue will negate all the effects of increased productivity.
    -----
    
    Your program will be run in the terminal once (include a loop if needed).
    At every step of the game (maximum of 1000 steps), it will receive a set of observations as input and it will have to output an action to be taken.
    The observations will be input as a space-separated list of 6 floating point observations: ['alertness', 'hypertension', 'intoxication',
     'time_since_slept', 'time_elapsed', 'work_done']
    
    In response, print ONLY the number of the action to be taken, from the list below:
    0 - just work
    1 - drink coffee and work
    2 - drink beer and work
    3 - sleep

--- metanurse.py
+++ metanurse.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def choose_action(observations):
+def decide_action(observations):
     (
         alertness,
         hypertension,
@@ -11,11 +11,11 @@ def choose_action(observations):
         work_done,
     ) = observations
 
-    if time_since_slept > 16 or alertness < 0.2:
+    if time_since_slept > 16 or alertness < 0.3:
         return 3  # sleep
-    elif alertness < 0.5 and intoxication < 0.3:
+    elif alertness < 0.7 and intoxication < 0.5:
         return 1  # drink coffee and work
-    elif hypertension < 0.5 and intoxication < 0.5:
+    elif hypertension < 0.6 and intoxication < 0.3:
         return 2  # drink beer and work
     else:
         return 0  # just work
@@ -25,5 +25,5 @@ for _ in range(1000):
     observations = list(map(float, sys.stdin.readline().strip().split()))
     if len(observations) != 6:
         break
-    action = choose_action(observations)
+    action = decide_action(observations)
     print(action)
