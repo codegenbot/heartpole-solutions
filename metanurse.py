0d2d1d4490a9b303dd9084eaf94a0090afbd8db5
beep boop <githubbot@vadim.me>
2024-11-23 00:34:30 +0100

    15 (0.0). Final reward: -99.96166777234231

--- metanurse.py
+++ metanurse.py
@@ -1,20 +1,13 @@
 import sys
 
 for line in sys.stdin:
-    (
-        alertness,
-        hypertension,
-        intoxication,
-        time_since_slept,
-        time_elapsed,
-        work_done,
-    ) = map(float, line.split())
-    
-    if hypertension > 0.5 or intoxication > 0.5 or time_since_slept > 12:
+    alertness, hypertension, intoxication, time_since_slept, time_elapsed, work_done = map(float, line.split())
+
+    if hypertension > 0.6 or intoxication > 0.5 or time_since_slept > 14:
         print(3)  # sleep
-    elif alertness < 0.5:
+    elif alertness < 0.4 and hypertension <= 0.6:
         print(1)  # drink coffee and work
-    elif intoxication < 0.5:
+    elif intoxication < 0.2 and alertness < 0.7:
         print(2)  # drink beer and work
     else:
         print(0)  # just work
