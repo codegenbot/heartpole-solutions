23ccc29b73119abfb65a346b16135b4fe8ca8631
beep boop <githubbot@vadim.me>
2024-11-23 08:46:57 +0100

    227 (0.0). Final reward: -99.78731224841553

--- metanurse.py
+++ metanurse.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def choose_action(observations):
+def decide_action(observations):
     (
         alertness,
         hypertension,
@@ -12,16 +12,21 @@ def choose_action(observations):
     ) = observations
 
     if (
-        time_since_slept > 5
-        or alertness < 0.05
-        or hypertension > 0.7
+        time_since_slept > 4
+        or alertness < 0.2
+        or hypertension > 0.3
         or intoxication > 0.02
     ):
-        return 3  # sleep
-    elif alertness < 0.2 and intoxication < 0.01 and hypertension < 0.3:
+        return 3  # sleep to recover
+    elif 0.2 < alertness < 0.4 and intoxication < 0.01 and hypertension < 0.1:
         return 1  # drink coffee and work
-    elif work_done < 0.2 and intoxication < 0.01 and hypertension < 0.3:
-        return 2  # drink beer and work
+    elif (
+        alertness > 0.4
+        and intoxication < 0.01
+        and hypertension < 0.15
+        and work_done < 0.6
+    ):
+        return 0  # just work
     else:
         return 0  # just work
 
@@ -30,5 +35,5 @@ for _ in range(1000):
     observations = list(map(float, sys.stdin.readline().strip().split()))
     if len(observations) != 6:
         break
-    action = choose_action(observations)
+    action = decide_action(observations)
     print(action)
