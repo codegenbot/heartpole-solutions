d49c7f3d1545a0743ecb05e3276312b5c0ae15a9
beep boop <githubbot@vadim.me>
2024-11-23 01:57:54 +0100

    59 (0.0). Final reward: -99.70134494603312

--- metanurse.py
+++ metanurse.py
@@ -1,24 +1,22 @@
 import sys
 
-def decide_action(alertness, hypertension, intoxication, time_since_slept, time_elapsed, work_done):
-    if hypertension > 0.7 or intoxication > 0.4:
-        return 3  # Immediate sleep if critical health issues are significant
-    if alertness < 0.4:
-        if time_elapsed < 8:
-            return 1  # Drink coffee if it's within working hours
-        else:
-            return 3  # Sleep if alertness is low and it's late
-    if time_since_slept > 10:
-        return 3  # Encourage sleep after long wake periods
-    if time_since_slept > 6 and alertness < 0.5:
-        return 3  # Sleep if moderate alertness but long awake time
-    if work_done < 0.5 and alertness > 0.7:
-        return 0  # Just work if work is lacking but alertness is high
-    if alertness < 0.7 and time_elapsed < 8:
-        return 1  # Drink coffee and work to boost productivity
-    return 0  # Default to just work
-
 for line in sys.stdin:
-    observations = list(map(float, line.strip().split()))
-    action = decide_action(*observations)
-    print(action)
+    (
+        alertness,
+        hypertension,
+        intoxication,
+        time_since_slept,
+        time_elapsed,
+        work_done,
+    ) = map(float, line.split())
+
+    if intoxication > 0.3 or hypertension > 0.5 or time_since_slept > 15:
+        print(3)  # sleep
+    elif alertness < 0.6 and time_since_slept < 10 and hypertension < 0.25:
+        print(1)  # drink coffee and work
+    elif alertness < 0.8 and 0.1 <= intoxication <= 0.3 and hypertension < 0.3:
+        print(2)  # drink beer and work
+    elif alertness >= 0.7 and hypertension < 0.3 and intoxication < 0.1:
+        print(0)  # just work
+    else:
+        print(3)  # sleep if unsure
