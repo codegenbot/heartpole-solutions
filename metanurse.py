490370064bf0be34301d43b96bee9967684a5df0
beep boop <githubbot@vadim.me>
2024-11-23 13:50:15 +0100

    428 (0.0). Final reward: -99.92790457833415

--- metanurse.py
+++ metanurse.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def decide_action(
     alertness, hypertension, intoxication, time_since_slept, time_elapsed, work_done
 ):
@@ -21,6 +22,7 @@ def decide_action(
 
     # Default to working if conditions are optimal
     return 0  # Just work
+
 
 for line in sys.stdin:
     observations = list(map(float, line.strip().split()))
