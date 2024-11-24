a3a1d4d6628cf10b7ca522a257c6f3ddce6eb451
beep boop <githubbot@vadim.me>
2024-11-24 23:14:26 +0100

    1463 (0.0). Final reward: 0.2523706479575061

--- metanurse.py
+++ metanurse.py
@@ -1,20 +1,21 @@
 import sys
 
-def decide_action(alertness, hypertension, intoxication, time_since_slept, time_elapsed, work_done):
-    # Prioritize sleep more aggressively
-    if alertness < 0.5 or hypertension > 0.7 or intoxication > 0.5 or time_since_slept > 7:
-        return 3  # Sleep to recover
+def decide_action(
+    alertness, hypertension, intoxication, time_since_slept, time_elapsed, work_done
+):
+    if time_since_slept > 7 or hypertension > 0.65 or intoxication > 0.55 or alertness < 0.3:
+        return 3  # Must sleep
 
-    if alertness > 0.85 and hypertension < 0.5 and intoxication < 0.2:
+    if alertness > 0.8 and hypertension < 0.25 and intoxication < 0.2:
         return 0  # Just work
 
-    if 0.65 <= alertness <= 0.85 and hypertension < 0.55 and intoxication < 0.25:
-        return 1  # Drink coffee and work
+    if 0.5 <= alertness <= 0.8 and hypertension < 0.4 and intoxication < 0.3:
+        return 1  # Drink coffee
 
-    if 0.6 <= alertness < 0.65 and intoxication < 0.15:
-        return 2  # Drink beer and work
+    if 0.3 <= alertness < 0.5 and hypertension < 0.3 and intoxication < 0.45:
+        return 2  # Drink beer
 
-    return 3  # Default to sleep for safety
+    return 3  # Default to sleep if none of the above
 
 for line in sys.stdin:
     observations = list(map(float, line.strip().split()))
