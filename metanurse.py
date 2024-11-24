c6b9e7e09090036f6aacd243f24f89c5f224398f
beep boop <githubbot@vadim.me>
2024-11-24 01:51:14 +0100

    800 (0.0). Final reward: 0.22923665048703473

--- metanurse.py
+++ metanurse.py
@@ -1,6 +1,5 @@
 import sys
 
-
 def decide_action(
     alertness, hypertension, intoxication, time_since_slept, time_elapsed, work_done
 ):
@@ -26,7 +25,6 @@ def decide_action(
 
     # Default to working
     return 0
-
 
 for line in sys.stdin:
     observations = list(map(float, line.strip().split()))
