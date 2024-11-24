6e9a01c0fa140fe540e993705db34c72d3ca95ee
beep boop <githubbot@vadim.me>
2024-11-24 08:34:46 +0100

    1011 (0.0). Final reward: -99.68603459513707

--- metanurse.py
+++ metanurse.py
@@ -1,17 +1,16 @@
 import sys
 
-def decide_action(
-    alertness, hypertension, intoxication, time_since_slept, time_elapsed, work_done
-):
-    if time_since_slept > 7 or hypertension >= 0.6 or intoxication >= 0.5:
+def decide_action(alertness, hypertension, intoxication, time_since_slept, time_elapsed, work_done):
+    # Prioritize health parameters:
+    if alertness < 0.8 or time_since_slept > 6:
         return 3
-    if alertness < 0.5 or time_since_slept > 5:
+    if hypertension > 0.4 or intoxication > 0.25:
         return 3
-    if alertness < 0.7 and hypertension < 0.5:
+    
+    # Balance productivity:
+    if alertness < 0.9 and hypertension <= 0.3 and intoxication < 0.1:
         return 1
-    if alertness >= 0.7 and hypertension < 0.4 and intoxication < 0.25:
-        return 0
-    return 2 if intoxication < 0.3 else 0
+    return 0
 
 for line in sys.stdin:
     observations = list(map(float, line.strip().split()))
