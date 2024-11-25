f1e72c0af1a97401bf56edc34648bd5ba36e6ece
beep boop <githubbot@vadim.me>
2024-11-25 11:26:42 +0100

    1844 (0.0). Final reward: -99.80381186228205

--- metanurse.py
+++ metanurse.py
@@ -1,7 +1,7 @@
 import sys
 
 def decide_action(alertness, hypertension, intoxication, time_since_slept, time_elapsed, work_done):
-    # Prioritize sleep for high health risks
+    # Priortize sleep for high health risks
     if hypertension > 0.3 or intoxication > 0.2:
         return 3
     # Prioritize sleep if low alertness or long time without sleep
