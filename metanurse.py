a5321db1930000587ca130130b4d3e3a98284d3b
beep boop <githubbot@vadim.me>
2024-11-25 08:14:41 +0100

    1445 (0.0). Final reward: -99.84488242922662

--- metanurse.py
+++ metanurse.py
@@ -13,19 +13,19 @@ def choose_action(observations):
 
     # Prioritize sleep for critical health conditions
     if (
-        hypertension > 0.04
+        hypertension > 0.03
         or intoxication > 0.0003
-        or time_since_slept > 2.5
-        or alertness < 0.15
+        or time_since_slept > 2
+        or alertness < 0.1
     ):
         return 3  # sleep
 
     # Drink coffee if alertness is low and health conditions are okay
-    if alertness < 0.25 and intoxication < 0.0003 and hypertension < 0.02:
+    if alertness < 0.2 and intoxication < 0.0003 and hypertension < 0.02:
         return 1  # drink coffee and work
 
     # Drink beer if work done is low and health conditions are okay
-    if work_done < 0.4 and intoxication < 0.0003 and hypertension < 0.02:
+    if work_done < 0.2 and intoxication < 0.0003 and hypertension < 0.02:
         return 2  # drink beer and work
 
     return 0  # just work
