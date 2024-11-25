e605749c18e02da1fb51ef51df7489dd2a81771c
beep boop <githubbot@vadim.me>
2024-11-25 23:40:33 +0100

    2222 (0.0). Final reward: 0.08283142527179653

--- metanurse.py
+++ metanurse.py
@@ -9,16 +9,16 @@ def decide_action(
         or intoxication > 0.04
         or time_since_slept >= 3.0
     ):
-        return 3
+        return 3  # Sleep whenever alertness is low or health indicators are high
 
     if 0.5 <= alertness < 0.75 and hypertension < 0.05 and intoxication < 0.03:
-        return 1
+        return 1  # Drink coffee to boost alertness mildly within safe parameters
 
     if alertness >= 0.75 and hypertension < 0.03 and intoxication < 0.02:
-        return 0
+        return 0  # Optimal conditions for working
 
     if work_done < 0.01 and intoxication <= 0.01 and alertness < 0.3:
-        return 2
+        return 2  # Beer can help stimulate extremely low productivity over time
 
     return 0
 
