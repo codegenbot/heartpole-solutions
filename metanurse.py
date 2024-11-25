462031ac8a24453f5d673cf04a3af56fd1de04d3
beep boop <githubbot@vadim.me>
2024-11-25 10:41:00 +0100

    1820 (0.0). Final reward: -99.77993438057278

--- metanurse.py
+++ metanurse.py
@@ -1,10 +1,10 @@
 import sys
 
 def decide_action(alertness, hypertension, intoxication, time_since_slept, time_elapsed, work_done):
-    # Prioritize sleep if moderate health levels are met or work is excessive.
+    # Prioritize sleep if any health indicators are at moderate levels or work is excessive.
     if hypertension >= 0.25 or intoxication >= 0.05 or alertness <= 0.65 or time_since_slept >= 5 or work_done >= 0.8:
         return 3
-    # Use coffee if alertness is low but not near sleep requirement or high hypertension
+    # Use coffee if alertness is low but not when near sleep requirement or high hypertension
     if alertness < 0.75 and hypertension < 0.2 and time_since_slept < 4:
         return 1
     # Default action is just work
