a43c882d5364cf6630525d3fbc0799af77ba079e
beep boop <githubbot@vadim.me>
2024-11-25 09:37:45 +0100

    1481 (0.0). Final reward: 0.12305351375470956

--- metanurse.py
+++ metanurse.py
@@ -15,14 +15,14 @@ def choose_action(observations):
         hypertension > 0.02
         or intoxication > 0.0001
         or time_since_slept > 1.5
-        or alertness < 0.05
+        or alertness < 0.15
     ):
         return 3  # sleep
 
-    if alertness < 0.15 and intoxication < 0.0001 and hypertension < 0.005:
+    if alertness < 0.25 and intoxication < 0.0001 and hypertension < 0.01:
         return 1  # drink coffee and work
 
-    if work_done < 0.2 and intoxication < 0.0001 and hypertension < 0.005:
+    if work_done < 0.4 and intoxication < 0.0001 and hypertension < 0.01:
         return 2  # drink beer and work
 
     return 0  # just work
