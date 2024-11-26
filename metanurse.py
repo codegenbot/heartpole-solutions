2b4728716af29f0b30040901049b5b72f0ac8194
beep boop <githubbot@vadim.me>
2024-11-26 17:41:46 +0100

    2797 (0.0). Final reward: 0.20311063512277727

--- metanurse.py
+++ metanurse.py
@@ -3,19 +3,20 @@ import sys
 def decide_action(
     alertness, hypertension, intoxication, time_since_slept, time_elapsed, work_done
 ):
-    # Adjusted health risk management:
-    if hypertension >= 0.05 or intoxication >= 0.06:
+    # Strict health risk management:
+    if hypertension > 0.05 or intoxication > 0.06:
         return 3
+
     if time_since_slept > 6:
         return 3
 
-    # Adjusted caffeine intake management:
-    if alertness < 0.65:
-        if hypertension < 0.035 and intoxication < 0.035:
+    # If alertness is low and safe conditions for coffee are met:
+    if alertness < 0.7:
+        if hypertension < 0.04 and intoxication < 0.04:
             return 1
 
-    # Optimize working conditions:
-    if alertness >= 0.75 and hypertension < 0.025 and intoxication < 0.025:
+    # Optimize working conditions while accepting moderate risks:
+    if alertness > 0.75 and hypertension < 0.03 and intoxication < 0.03:
         return 0
 
     # Default to sleeping to restore health and alertness:
