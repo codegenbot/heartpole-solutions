0a0e03f0f69e0c0582c09e2cbe205b610644f91b
beep boop <githubbot@vadim.me>
2024-11-26 09:28:57 +0100

    2535 (0.0). Final reward: 0.1533832448294352

--- metanurse.py
+++ metanurse.py
@@ -4,17 +4,25 @@ import sys
 def decide_action(
     alertness, hypertension, intoxication, time_since_slept, time_elapsed, work_done
 ):
-    if hypertension > 0.02 or intoxication > 0.05:
+    if (
+        alertness < 0.4
+        or hypertension >= 0.03
+        or intoxication > 0.15
+        or time_since_slept >= 7
+    ):
         return 3
-    if time_since_slept >= 6:
-        return 3
-    if alertness < 0.5:
-        return 3
-    if alertness < 0.6 and hypertension < 0.01:
+    if (
+        0.4 <= alertness < 0.7
+        and hypertension < 0.02
+        and intoxication < 0.1
+        and time_since_slept < 5
+    ):
         return 1
-    if 0.6 <= alertness < 0.75 and hypertension < 0.01:
+    if alertness >= 0.7 and hypertension < 0.02 and intoxication < 0.1:
         return 0
-    return 3
+    if intoxication >= 0.1 and hypertension < 0.025:
+        return 2
+    return 0
 
 
 for line in sys.stdin:
