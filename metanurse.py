a4cdff32d87b94ca20dd66976d3570ec9f7ab72d
beep boop <githubbot@vadim.me>
2024-11-26 06:54:05 +0100

    2452 (0.0). Final reward: -99.84357744654581

--- metanurse.py
+++ metanurse.py
@@ -1,10 +1,7 @@
 import sys
 
-
-def decide_action(
-    alertness, hypertension, intoxication, time_since_slept, time_elapsed, work_done
-):
-    # Prioritize sleep if attention is significantly compromised, intoxication is serious, or it's been a long time since last sleep
+def decide_action(alertness, hypertension, intoxication, time_since_slept, time_elapsed, work_done):
+    # Prioritize sleep if attention is significantly compromised, intoxication is serious or it's been a long time since last sleep
     if alertness < 0.7 or time_since_slept >= 3 or intoxication > 0.05:
         return 3
     # Drink coffee only if it’s safe and necessary to boost moderate alertness
@@ -15,7 +12,6 @@ def decide_action(
         return 2
     # Default to work only when in a safe state
     return 0
-
 
 for line in sys.stdin:
     observations = list(map(float, line.strip().split()))
